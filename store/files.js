ff8799d720e2ddc3480a7ec94ba4bab7942861fe
Ali Talebi <frostcyber93@gmail.com>
2021-03-22 16:45:42 +0430

    File

---
+++ store/files.js
@@ -0,0 +1,42 @@
+export const state = () => {
+  return {
+    file: {}
+  }
+}
+
+export const mutations = {
+  SET_FILE(state, file) {
+    state.file = file
+  }
+}
+
+export const actions = {
+  async uploadProductImage({commit}, file) {
+    let fileData = new FormData
+    fileData.append('file', file)
+    let data = null
+    await this.$apiClient.post('api/files/upload?for=ProductImage', fileData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(res => data = res.data).catch(err => console.log(err))
+    commit('SET_FILE' , data)
+  },
+  async uploadEditorImage({commit}, file) {
+    let fileData = new FormData
+    fileData.append('file', file)
+    let data = null
+    await this.$apiClient.post('api/files/upload?for=EditorImage', fileData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(res => data = res.data).catch(err => console.log(err))
+    commit('SET_FILE' , data)
+  }
+}
+
+export const getters = {
+  getFile(state){
+    return JSON.parse(JSON.stringify(state.file))
+  }
+}
