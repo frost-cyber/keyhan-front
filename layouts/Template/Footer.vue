ecfb1073127092f97855f45db4ed51f2c89301a3
Ali Talebi <frostcyber93@gmail.com>
2021-03-25 11:16:50 +0430

    init layout

---
+++ layouts/Template/Footer.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="footer-row mt-14 relative">
+    <footer class="footer py-9 bg-cool-100">
+      <div class="container mx-auto">
+        <div class="grid grid-cols-12 gap-30">
+          <div class="col-span-12 md:col-span-6 lg:col-span-4 text-center order-1 md:order-1 lg:order-1">
+            <div class="title-footer">
+              <h4 class=" relative text-xl text-center text-cool-800 font-bold pb-2 border-b-2 border-cool-700 inline-block px-5 title-border">
+                اطلاعات تماس با ما
+              </h4>
+            </div>
+            <div class="info-box text-right bg-ff bg-opacity-50 rounded-lg shadow-xl mt-4 p-3">
+              <div class="bg-cool-50 rounded-lg p-4 text-cool-500 shadow-my ">
+                <figure class="mb-3">
+                  <i class="fal fa-map-marker-alt text-2xl ml-2"></i>
+                  <span class="text-sm font-thin">
+                    بیرجند - خیابان شهدا - شهدا 5 - پلاک 2 -واحد 12
+                  </span>
+                </figure>
+                <figure class="mb-3">
+                  <i class="fal fa-phone text-2xl ml-2"></i>
+                  <span class="text-sm font-thin"> 056-32210006</span>
+                </figure>
+                <figure class="mb-3">
+                  <i class="fal fa-phone text-2xl ml-2"></i>
+                  <span class="text-sm font-thin"> 056-32210007</span>
+                </figure>
+                <figure class="mb-3 pb-4 border-b border-dashed border-cool-400">
+                  <i class="fal fa-mobile text-2xl ml-2"></i>
+                  <span class="text-sm font-thin"> 09120760345</span>
+                </figure>
+                <div class="social-btn text-center">
+                  <a class="mx-2 text-2xl shadow-neon p-2.5 w-11 h-11 text-center rounded-lg text-cool-400 bg-cool-100 inline-block" href="">
+                    <i class="fab fa-whatsapp "></i>
+                  </a>
+                  <a class="mx-2 text-2xl shadow-neon p-2.5 w-11 h-11 text-center rounded-lg text-cool-400 bg-cool-100 inline-block" href="">
+                    <i class="fab fa-instagram "></i>
+                  </a>
+                  <a
+                    class="mx-2 text-2xl shadow-neon p-2 w-11 h-11 text-center rounded-lg text-cool-400 bg-cool-100 inline-block"
+                    href=""
+                  ><i class="fal fa-paper-plane "></i
+                  ></a>
+                  <a
+                    class="mx-2 inline-block mt-0 shadow-neon p-2 w-11 h-11 text-center  rounded-lg relative top-3.5 bg-cool-100"
+                    href=""
+                  ><img class="" src="@/assets/img/aparat.svg" alt=""
+                  /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="col-span-12 md:col-span-12 lg:col-span-4 text-center lg:flex lg:items-end align-bottom order-2 md:order-3 lg:order-2"
+          >
+
+            <div class="bax-namadha">
+              <div class="backtotop mb-14 sticky bottom-10 right-3 z-10 m-auto block">
+                <a
+                  id="button"
+                  class="mx-2 text-2xl shadow-neon p-2 w-11 h-11 text-center rounded-lg text-cool-400 bg-cool-100 inline-block"
+                  href=""
+                ><i class="fal fa-arrow-up "></i
+                ></a>
+              </div>
+              <div class="title-footer">
+                <h4
+                  class=" relative text-xl text-center text-cool-800 font-bold pb-2 border-b-2 border-cool-700 inline-block px-5 title-border"
+                >
+                  نمادها و مجوزها
+                </h4>
+              </div>
+              <div
+                class="info-box text-right bg-ff bg-opacity-50 rounded-lg shadow-xl mt-4 p-3"
+              >
+                <div class="bg-cool-50 rounded-lg p-4 text-cool-500 shadow-my">
+                  <div class="img-namadha">
+                    <img class="m-auto" src="@/assets/img/amniyat.png" alt=""/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-12 md:col-span-6 lg:col-span-4 text-center order-3 md:order-2 lg:order-3">
+            <div class="title-footer">
+              <h4
+                class=" relative text-xl text-center text-cool-800 font-bold pb-2 border-b-2 border-cool-700 inline-block px-5 title-border"
+              >
+                پیونـــدها و لینک ها
+              </h4>
+            </div>
+            <div
+              class="info-box text-right bg-ff bg-opacity-50 rounded-lg shadow-xl mt-4 p-3"
+            >
+              <div class="bg-cool-50 rounded-lg p-4 text-cool-500 shadow-my ">
+                <div class="footer-menu">
+                  <ul>
+                    <li>
+                      <a href="">
+                        ارتباط با ما
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        تماس با ما
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        قوانین و مقررات
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        مرجوعی
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        غرفه ما در دیجی کالا
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        غرفه ما در باسلام
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        کلید و پریزهای لمسی
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        پنل های خورشیدی
+                      </a>
+                    </li>
+                    <li>
+                      <a href="">
+                        برندهای موجود در صنعت برق
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <section class="copyright py-4">
+      <div class="container mx-auto">
+        <div class="grid grid-cols-2 gap-30">
+          <div class="col-span-2 md:col-span-1 md:flex md:items-center">
+            <p
+              class="text-center md:text-right text-sm font-light text-cool-600"
+            >
+              تمامی حقوق کپی رایت نزدی ایناست
+            </p>
+          </div>
+          <div class="col-span-2 md:col-span-1">
+            <p
+              class="text-center md:text-left text-sm font-light text-cool-600"
+            >
+              طراحی و توسعه توسط
+              <a class="inline-block " href="https://viranika.com/">
+                <img
+                  class="inline-block"
+                  src="@/assets/img/viranika.svg"
+                  alt=""
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+</template>
+
---
+++ layouts/Template/Header.vue
@@ -0,0 +1,152 @@
+<template>
+  <header id="#header" class="header-area">
+    <section class="header py-8 bg-cool-50 ">
+      <div class="container m-auto">
+        <div class="grid grid-cols-12 gap-30">
+          <div
+            class="col-span-12 md:col-span-2 lg:col-span-1 text-center md:text-right m-auto logo"
+          >
+            <div class="logo-area">
+              <a class="m-auto" href="">
+                <img src="../assets/img/logo.svg" alt="لوگو کیهان" />
+              </a>
+            </div>
+          </div>
+          <div
+            class="col-span-12 md:col-span-5 lg:col-span-5 font-fd my-auto mr-0 lg:mr-5 search"
+          >
+            <vs-input
+              color="#F97316"
+              icon-after="true"
+              icon-pack="fal"
+              icon="fa-search"
+              placeholder="جستجو ..."
+              :icon-no-border="false"
+              v-model="value1"
+            />
+          </div>
+          <div
+            class="col-span-12 md:col-span-5 lg:col-span-6 my-auto text-center md:text-left sign"
+          >
+            <div class="btnlogin inline-block ml-3">
+              <a class="btn-border" href="">ورود به حساب کاربری</a>
+            </div>
+            <div class=" btncart inline-block relative">
+              <a
+                class="p-2 h-16 w-16 bg-ff border border-cool-100 rounded-lg"
+                href="/"
+              >
+                <span>
+                  <i
+                    class="fal fa-shopping-cart text-xl text-cool-400 relative top-1"
+                  ></i>
+                </span>
+              </a>
+              <span class="num-cart absolute font-fd">6</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="navmenu py-4 border-cool-100 border">
+      <div class="container m-auto">
+        <div class="grid grid-cols-12 gap-30 ">
+          <div class="col-span-6 lg:col-span-10">
+            <div class="btn-collaps inline-block">
+              <a class="px-6 py-2 bg-cool-600 text-ff rounded-lg" href="#"
+                >دسته بندی ها<i class="mr-2 text-cool-50 fal fa-bars"></i
+              ></a>
+              <div class="mega-araea">
+                <vs-tabs :color="colorx" position="left">
+                  <vs-tab @click="colorx = 'success'" label="Success">
+                    <div class="con-tab-ejemplo">
+                      Success
+                    </div>
+                  </vs-tab>
+                  <vs-tab @click="colorx = 'danger'" label="Danger">
+                    <div class="con-tab-ejemplo">
+                      Danger
+                    </div>
+                  </vs-tab>
+                  <vs-tab @click="colorx = 'warning'" label="Warning">
+                    <div class="con-tab-ejemplo">
+                      Warning
+                    </div>
+                  </vs-tab>
+                  <vs-tab @click="colorx = 'dark'" label="Dark">
+                    <div class="con-tab-ejemplo">
+                      Dark
+                    </div>
+                  </vs-tab>
+                  <vs-tab
+                    @click="colorx = 'rgb(16, 233, 179)'"
+                    label="RGB | HEX"
+                  >
+                    <div class="con-tab-ejemplo">
+                      RGB | HEX
+                    </div>
+                  </vs-tab>
+                </vs-tabs>
+              </div>
+            </div>
+            <div class="main-menu inline-block text-base ">
+              <ul>
+                <li>
+                  <a href="" class="text-cool-800 hover:text-orange-600 ">
+                    صفحه اصلی
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="text-cool-800 hover:text-orange-600 ">
+                    درباره ما
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="text-cool-800 hover:text-orange-600 ">
+                    تماس با ما
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="text-cool-800 hover:text-orange-600 ">
+                    با ما همکار شو
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="text-cool-800 hover:text-orange-600 ">
+                    وبلاگ
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="text-orange-500">
+                    جشنواره
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="lg:col-span-2 lg:text-left">
+            <figure>
+              <span class="font-thin text-cool-600">09120760345</span>
+              <i class="fal fa-phone mr-2 text-xl text-cool-600"></i>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+  </header>
+</template>
+<script>
+//import carousel from 'vue-owl-carousel'
+//import HelloWorld from "./HelloWorld.vue";
+export default {
+  data() {
+    return {
+      value1: "",
+      colorx: "success",
+    };
+  },
+  components: {
+    // HelloWorld: HelloWorld,
+  },
+};
+</script>
