72897e9fa33058a4e704bc093997e048e3e189aa
ftm.sh <f.shokatabadi78@gmail.com>
2021-05-23 12:56:05 +0430

    component profile

---
+++ components/front/profile/editmyaccount.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "editmyaccount",
+      props:{
+        users: {
+          required: true,
+        },
+      },
+      methods:{
+        updateProfile() {
+              this.$store.commit('profile/SET_USER', this.users)
+            this.$emit('update-profile')
+
+
+        },
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/front/profile/factororder.vue
@@ -0,0 +1,194 @@
+<template>
+    <div>
+      <div class="title-section mb-6 relative">
+        <h3
+          class="title-border relative text-base font-black text-cool-800 inline-block border-b-2 border-cool-800 pb-2 pl-10 mt-8"
+        >
+          فاکتور این سفارش
+        </h3>
+      </div>
+      <div class="factor-area rounded-lg border border-cool-200 px-5 pb-6">
+        <div class="title-item grid-cols-6 gap-4 py-3 hidden lg:grid">
+          <div class="col-span-12 md:col-span-3 text-center">
+            <h5 class="h-5">نام محصول</h5>
+          </div>
+          <div class="col-span-6 md:col-span-1 text-center ">
+            <h5>قیمت واحد</h5>
+          </div>
+          <div class="col-span-6 md:col-span-1  text-center">
+            <h5> تعداد</h5>
+          </div>
+          <div class="col-span-12 md:col-span-1  text-center">
+            <h5>قیمت کل</h5>
+          </div>
+        </div>
+        <div class="cart-item grid grid-cols-6 gap-4 border-b border-cool-400 border-dashed py-4">
+          <div class="col-span-6 md:col-span-6 lg:col-span-3">
+            <div class="product-cart grid grid-cols-12 gap-2 border-b pb-2 border-dashed border-cool-200 md:pb-0 md:border-b-0">
+
+              <div class="col-span-4 md:col-span-5 lg:col-span-4">
+                <a href="">
+                  <img class="pimg-cart w-full border-cool-200 border rounded-lg" src="@/assets/img/product/img-p02.jpg" alt="">
+                </a>
+              </div>
+              <div class="col-span-8 md:col-span-7 lg:col-span-8">
+                <a href="">
+                  <h3 class="ptitle-cart text-cool-600 text-sm font-bold inline-block">B13IMICMSXJ دوربین مداربسته تحت شبکه شیائومی مدل</h3>
+                </a>
+                <div class="sp-cart" >
+                  <span class="text-xs text-cool-500">طول</span>
+                  <span class="text-xs text-cool-500"> 500متری</span>
+                </div>
+                <div class="sp-cart inline-block " >
+                  <span class="text-xs text-cool-500">رنگ</span>
+                  <span class="text-xs text-cool-500"> <button class=" w-4 h-4 border-blue-400 border-2 rounded-3xl mr-1 relative lg:-top-1 top-0"></button></span>
+                </div>
+                <div class="holesale-tag lg:float-left mt-1.5"><p class="text-red-500 text-xs">عمده محاسبه شده است</p></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-6 md:col-span-6 lg:col-span-1 text-center ">
+            <div class="area-unitprice border-b pb-2 border-dashed md:border-b-0 border-cool-200 lg:border-l lg:border-r lg:border-dashed lg:border-cool-300 min-h-90 flex items-center align-middle">
+              <div class="block lg:hidden">
+                <h5>قیمت واحد: </h5>
+              </div>
+              <div class=" m-auto">
+                <span class=" m-auto text-cool-400 line-through block text-right">55,000,000</span>
+                <span class="text-center m-auto text-cool-800 inline-block ">35,000,000</span>
+                <span class="text-center m-auto text-xs text-cool-800 inline-block mr-1">تومان</span>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-span-6 md:col-span-6 lg:col-span-1 text-center">
+            <div class="unit-cart border-b pb-2 border-dashed md:border-b-0 border-cool-200 min-h-90 m-auto flex items-center align-middle">
+              <div class="block lg:hidden">
+                <h5>تعداد: </h5>
+              </div>
+              <div class=" text-center m-auto px-2">
+                <span class="text-cool-500 text-sm"> 5 عدد </span>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-6 md:col-span-6 lg:col-span-1 text-center">
+            <div class="hole-cart lg:border-r lg:border-dashed lg:border-cool-300 m-auto min-h-90 flex items-center align-middle">
+              <div class="block lg:hidden">
+                <h5>قیمت کل: </h5>
+              </div>
+              <div class=" m-auto">
+                <span class=" m-auto text-cool-400 line-through block text-right">55,000,000</span>
+                <span class="text-center m-auto  text-red-500 font-bold inline-block ">35,000,000</span>
+                <span class="text-center m-auto text-xs text-cool-800 inline-block mr-1">تومان</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="cart-item grid grid-cols-6 gap-4 border-b border-cool-400 border-dashed py-4">
+          <div class="col-span-6 md:col-span-6 lg:col-span-3">
+            <div class="product-cart grid grid-cols-12 gap-2 border-b pb-2 border-dashed border-cool-200 md:pb-0 md:border-b-0">
+
+              <div class="col-span-4 md:col-span-5 lg:col-span-4">
+                <a href="">
+                  <img class="pimg-cart w-full border-cool-200 border rounded-lg" src="@/assets/img/product/img-p02.jpg" alt="">
+                </a>
+              </div>
+              <div class="col-span-8 md:col-span-7 lg:col-span-8">
+                <a href="">
+                  <h3 class="ptitle-cart text-cool-600 text-sm font-bold inline-block">B13IMICMSXJ دوربین مداربسته تحت شبکه شیائومی مدل</h3>
+                </a>
+                <div class="sp-cart" >
+                  <span class="text-xs text-cool-500">طول</span>
+                  <span class="text-xs text-cool-500"> 500متری</span>
+                </div>
+                <div class="sp-cart inline-block " >
+                  <span class="text-xs text-cool-500">رنگ</span>
+                  <span class="text-xs text-cool-500"> <button class=" w-4 h-4 border-blue-400 border-2 rounded-3xl mr-1 relative lg:-top-1 top-0"></button></span>
+                </div>
+                <div class="holesale-tag lg:float-left mt-1.5"><p class="text-red-500 text-xs">عمده محاسبه شده است</p></div>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-6 md:col-span-6 lg:col-span-1 text-center ">
+            <div class="area-unitprice border-b pb-2 border-dashed md:border-b-0 border-cool-200 lg:border-l lg:border-r lg:border-dashed lg:border-cool-300 min-h-90 flex items-center align-middle">
+              <div class="block lg:hidden">
+                <h5>قیمت واحد: </h5>
+              </div>
+              <div class=" m-auto">
+                <span class=" m-auto text-cool-400 line-through block text-right">55,000,000</span>
+                <span class="text-center m-auto text-cool-800 inline-block ">35,000,000</span>
+                <span class="text-center m-auto text-xs text-cool-800 inline-block mr-1">تومان</span>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-span-6 md:col-span-6 lg:col-span-1 text-center">
+            <div class="unit-cart border-b pb-2 border-dashed md:border-b-0 border-cool-200 min-h-90 m-auto flex items-center align-middle">
+              <div class="block lg:hidden">
+                <h5>تعداد: </h5>
+              </div>
+              <div class=" text-center m-auto px-2">
+                <span class="text-cool-500 text-sm"> 5 عدد </span>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-6 md:col-span-6 lg:col-span-1 text-center">
+            <div class="hole-cart lg:border-r lg:border-dashed lg:border-cool-300 m-auto min-h-90 flex items-center align-middle">
+              <div class="block lg:hidden">
+                <h5>قیمت کل: </h5>
+              </div>
+              <div class=" m-auto">
+                <span class=" m-auto text-cool-400 line-through block text-right">55,000,000</span>
+                <span class="text-center m-auto  text-red-500 font-bold inline-block ">35,000,000</span>
+                <span class="text-center m-auto text-xs text-cool-800 inline-block mr-1">تومان</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="final-factor grid grid-cols-6 gap-30">
+          <div class="col-span-6 lg:col-start-4 lg:col-end-7 rounded-lg rounded-t-none bg-cool-100 text-cool-700 p-2 mt-1">
+
+            <div class="price-item h-7 ">
+              <span class="text-cool-600 text-sm font-normal float-right mt-0.5">
+                جمع کل سفارشات  :</span
+              >
+
+              <span class="text-base font-bold text-cool-500 mx-1 float-left"> 35,000,000</span>
+            </div>
+            <div class="price-item h-7 ">
+              <span class="text-cool-600 text-sm font-normal float-right mt-0.5">
+                میزان تخفیف  :</span
+              >
+
+              <span class="text-base font-bold text-red-500 mx-1 float-left"> 12000 - </span>
+            </div>
+            <div class="price-item h-7 ">
+              <span class="text-cool-600 text-sm font-normal float-right mt-0.5">
+                هزینه ارسال  :</span
+              >
+
+              <span class="text-base font-bold text-cool-500 mx-1 float-left"> 35,000,000</span>
+            </div>
+            <div class="price-item h-7 ">
+              <span class="text-cool-600 text-sm font-normal float-right mt-0.5">
+                جمع کل فاکتور  :</span
+              >
+
+              <span class="text-base font-bold text-green-500 mx-1 float-left"> 35,038,000</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "orderfactor"
+    }
+</script>
+
+<style scoped>
+
+</style>
