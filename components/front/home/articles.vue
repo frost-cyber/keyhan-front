cac978d84de2b55d2b0a4869a2e831d37473c0b5
Ali Talebi <frostcyber93@gmail.com>
2021-05-25 09:52:19 +0430

    settings

--- components/front/home/articles.vue
+++ components/front/home/articles.vue
@@ -3,20 +3,20 @@
     <template v-for="(article , index ) in articles">
       <article :key="index" class="overflow-hidden item-blog border border-cool-100 rounded-lg hover:shadow-xl mb-5 col-span-6 md:col-span-4 lg:col-span-3">
         <div class="img-blog">
-          <a class="img-thum" :href="article.href">
+          <nuxt-link class="img-thum" :to="{name: 'articles-slug' ,params:{slug:article.slug}}">
             <img :src="article.img" alt=""/>
-          </a>
+          </nuxt-link>
         </div>
         <div class="title-blog px-4 my-4 h-13 overflow-hidden">
           <a class="title-blog text-cool-800" href="">
             <h3 class="text-right font-black text-xs md:text-sm">
-              {{ article.title }}
+              {{ article.name }}
             </h3>
           </a>
         </div>
         <div class="des-course px-4 pb-4 h-15 overflow-hidden ">
           <p class="hidden md:block text-xs text-cool-600 font-thin text-right">
-            {{ article.desc }}
+            {{ article.description }}
           </p>
         </div>
       </article>
